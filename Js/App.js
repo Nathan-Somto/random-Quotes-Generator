79d1c6acf4e31d31d859fddf5d74289365f7ca5b
Mkparu <mkparusomtochi26@gmail.com>
2023-04-25 02:46:28 +0100

    rewrote code in es6 and added a loader

--- Js/App.js
+++ Js/App.js
@@ -1,16 +1,40 @@
 const generateQuote= document.querySelector(".btn");
 const quoteText =document.querySelector("#quoteText");
 const quoteAuthor =document.querySelector("#quoteAuthor");
+const loader = document.querySelector('.loader');
+const container = document.querySelector('.container');
+/**
+ * 
+ * @param {number} number 
+ * @returns a random number from 0 to number
+ */
 function random(number){
     return Math.floor(Math.random()*number);
 }
-function getQuote(){
-    let quoteNumber;
-fetch("https://type.fit/api/quotes").then((response)=>response.json())
-.then((data)=>{
-    quoteNumber=random(data.length);
-    quoteText.textContent=" "+data[quoteNumber].text;
-    quoteAuthor.textContent=data[quoteNumber].author;
-})
+
+/**
+ * @description - fetches a random quote in the array of objects and prints the quote and author.
+ */
+
+async function getQuote(){
+container.classList.add( 'none');
+loader.classList.remove('none');
+
+try{    
+let response = await fetch("https://type.fit/api/quotes");
+let data = await response.json();
+container.classList.remove( 'none');
+const{text,author} = data[random(data.length)];
+quoteText.textContent =`${text}`;
+quoteAuthor.textContent=`${author ?? 'no author'}`;
 }
+catch(err){
+    console.log(err);
+}
+finally{
+    loader.classList.add("none");
+}
+
+}
+
 generateQuote.addEventListener('click',getQuote);
